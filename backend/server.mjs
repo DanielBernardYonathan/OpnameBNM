18baa91659e3cbe2bf9ab4fe5e8ff7c501595637
DanielBernardYonathan <danielbyh10@gmail.com>
2025-11-25 15:42:22 +0700

    ganti link vercel

--- backend/server.mjs
+++ backend/server.mjs
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3001;
 
 // 3. Middleware
 const allowedOrigins = [
-  "https://opnamebnm.vercel.app",
+  "https://opname-bnm-three.vercel.app/",
   "http://localhost:3000", // Next.js dev
   "http://127.0.0.1:3000",
 ];
